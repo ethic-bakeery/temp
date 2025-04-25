c99e8f83bd8fd816a13721422a03edf94d04afa6
Abubakar Usman <98848465+ethic-bakeery@users.noreply.github.com>
2025-04-25 14:02:47 +0530

    Update intl.py

--- intl.py
+++ intl.py
@@ -4,3 +4,11 @@ def disable_internet():
     os.system("sudo ip route del default")
 
 disable_internet()
+
+
+import os
+
+def disable_network_interface(interface='eth0'):
+    os.system(f"sudo ifconfig {interface} down")
+
+disable_network_interface('wlan0')  # or 'eth0'
